a44d40431abd846df7406495c7acc94b06a6267d
RiddSona <164799496+RiddSona@users.noreply.github.com>
2024-04-01 15:17:42 +0100

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,8 @@
+let username = "Riddesh";
+let age = 26;
+let hobby = null;
+let job = undefined;
+
+
+    console.log(typeof hobby); //null's data type is object
+    console.log(typeof job); //undefined is a datatype in itself;
